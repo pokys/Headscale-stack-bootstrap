238e56b8c51c7a4a3037d14edb44412307b71af3
pokys <22177258+pokys@users.noreply.github.com>
2026-03-13 15:26:58 +0000

    Set admin PATH for bootstrap commands

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${PATH:-}"
 
 ROLE="${1:-}"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
